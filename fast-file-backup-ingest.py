ad7ab9409bf258280fd6f8c4f773aef9a66fa37a
Eric Powers <eric@mrpowers.com>
2025-01-12 22:33:43 -0800

    Initial version sqlite index.db created from recursively adding files.

---
+++ fast-file-backup-ingest.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import pathlib
+import sqlite3 as sql
+import sys
+
+#import psutil
+import getdents as gd
+
+def arg_isdir(arg):
+    path = os.path.abspath(arg)
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"directory: {arg} is not a valid directory")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', nargs='+', type=arg_isdir)
+    parser.add_argument('-db', '--database', type=pathlib.Path, default='index.db')
+    parser.add_argument('-q', '--query', action='store_true')
+    args = parser.parse_args()
+    query = args.query
+    paths = args.directory
+    db_path = args.database
+    BUFFER_SIZE = 2**24 # max(2**24, psutil.virtual_memory().free // 8)
+
+    DIRENT_TABLE_SCHEMA = (
+        ('id', 'INTEGER PRIMARY KEY'),
+        ('path', 'TEXT NOT NULL UNIQUE'),
+        ('type', 'INTEGER'),
+        ('inode', 'INTEGER'),
+        ('mode', 'INTEGER'),
+        ('dev', 'INTEGER'),
+        ('nlink', 'INTEGER'),
+        ('uid', 'INTEGER'),
+        ('gid', 'INTEGER'),
+        ('size', 'INTEGER'),
+        ('atime', 'INTEGER'),
+        ('mtime', 'INTEGER'),
+        ('ctime', 'INTEGER'),
+        ('atime_ns', 'INTEGER'),
+        ('mtime_ns', 'INTEGER'),
+        ('ctime_ns', 'INTEGER'),
+        ('blocks', 'INTEGER'),
+        ('rdev', 'INTEGER'),
+        #('flags', 'INTEGER'),
+        )
+
+    if query and not os.path.isfile(db_path):
+        parser.error(f"database: {db_path} is not a valid database")
+        sys.exit(1)
+
+    con = sql.connect(db_path)
+    cur = con.cursor()
+
+    if query:
+        for row in cur.execute("""SELECT path, type FROM DirEnt ORDER BY path LIMIT 100000"""):
+            path, type_ = row
+            print("{}{}".format(path, '/' if type_ == gd.DT_DIR else ''))
+        sys.exit(0)
+
+    def append_path_to_data(path, type_, data):
+        try:
+            stat = os.lstat(path)
+        except FileNotFoundError as e:
+            return
+        data.append((
+            path,
+            type_,
+            stat.st_ino, # inode
+            stat.st_mode,
+            stat.st_dev,
+            stat.st_nlink,
+            stat.st_uid,
+            stat.st_gid,
+            stat.st_size,
+            stat.st_atime,
+            stat.st_mtime,
+            stat.st_ctime,
+            stat.st_atime_ns,
+            stat.st_mtime_ns,
+            stat.st_ctime_ns,
+            stat.st_blocks,
+            stat.st_rdev,
+            #stat.st_flags,
+            ))
+
+    def execute_data(data):
+        for row in data:
+            cur.executemany("""INSERT OR IGNORE INTO DirEnt (path) VALUES (?)""", [row[:1]])
+            cur.executemany("""UPDATE DirEnt SET {} WHERE path=?""".format(
+                ",".join(["{}=?".format(e[0]) for e in DIRENT_TABLE_SCHEMA[2:]])),
+                [row[1:] + row[:1]])
+        con.commit()
+
+    # create table
+    cur.execute("""CREATE TABLE IF NOT EXISTS DirEnt ({})""".format(
+        ", ".join(" ".join(e) for e in DIRENT_TABLE_SCHEMA)))
+    con.commit()
+
+    # add directory paths from command line
+    data = []
+    for path in paths:
+        append_path_to_data(path, gd.DT_DIR, data)
+    execute_data(data)
+
+    # recursively search paths updating the database
+    current_id = -1
+    while True:
+        res = cur.execute("""SELECT id, path, type FROM DirEnt WHERE id > {} ORDER BY id LIMIT 1""".format(current_id))
+        row = res.fetchone()
+        if row is None:
+            break
+        current_id, path, type_ = row
+
+        data = []
+        append_path_to_data(path, type_, data)
+
+        if type_ == gd.DT_DIR:
+            for inode, type_, name in gd.getdents(path, BUFFER_SIZE):
+                name = os.path.join(path, name)
+                append_path_to_data(name, type_, data)
+
+        execute_data(data)
